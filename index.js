7d0783410599dd66ab6e1f75928d116448ebbc8d
kismail261 <71590279+kismail261@users.noreply.github.com>
2022-07-10 21:16:28 +0500

    a

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {PersistGate} from 'redux-persist/integration/react';
 import {Provider} from 'react-redux';
 /**
- * @format
+ * @format 
  */
 import React from 'react';
 import {StripeProvider} from '@stripe/stripe-react-native';
